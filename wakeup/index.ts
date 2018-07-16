b10986a86fea92372be2aa6d31a45709af9ece3e
Cody Hoover <git@codyhoover.com>
2018-07-16 22:35:00 +0200

    Add a wakeup function

---
+++ wakeup/index.ts
@@ -0,0 +1,9 @@
+import { Context, HttpRequest } from 'azure-functions-ts-essentials';
+
+export async function run(context: Context, req: HttpRequest) {
+        context.res = {
+            status: 200,
+            body: '',
+        };
+        return Promise.resolve();
+};
