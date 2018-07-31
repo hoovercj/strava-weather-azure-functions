70b2326d471731cba2b908c61e96f8a8101e4fd2
Cody Hoover <git@codyhoover.com>
2018-07-31 21:11:59 +0200

    Add function for viewing subscriptions

---
+++ view-subscription/index.ts
@@ -0,0 +1,47 @@
+import * as request from 'request-promise-native';
+import { Context, HttpRequest } from 'azure-functions-ts-essentials';
+
+import {
+    getStravaClientId,
+    getStravaClientSecret,
+    getHostedUrl,
+    getStravaWebhooksVerifyToken,
+    getStravaWebhooksToken
+} from '../shared/env';
+import {
+    handleGenericError,
+} from '../shared/function-utilities';
+import { getUrlWithParams } from '../shared/utilities';
+
+
+export async function run(context: Context, req: HttpRequest) {
+
+    try {
+        const stravaResponse: any = await createSubscription();
+
+        context.res = {
+            status: 200,
+            body: stravaResponse,
+        };
+        return Promise.resolve();
+    } catch {
+        return handleGenericError(context);
+    }
+};
+
+const createSubscription = async () => {
+    const stravaBaseUrl = 'https://api.strava.com/api/v3/push_subscriptions';
+    const params = {
+        client_id: getStravaClientId(),
+        client_secret: getStravaClientSecret(),
+    }
+
+    const url = getUrlWithParams(stravaBaseUrl, params);
+
+    try {
+        return request.get(url);
+    } catch (error) {
+        // TODO: logging
+        throw error;
+    }
+}
